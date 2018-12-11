9d56c30a54d55097c29cb4602f120108b59caa4e
jaeja <akobije@yahoo.com>
2018-12-11 14:20:55 +0000

    Updates

---
+++ precache-manifest.dddf8e071f8556308016379689521394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "7a26aed962bcc7026c42",
+    "url": "/robofriends/static/js/main.7a26aed9.chunk.js"
+  },
+  {
+    "revision": "25119274e15678301741",
+    "url": "/robofriends/static/js/1.25119274.chunk.js"
+  },
+  {
+    "revision": "7a26aed962bcc7026c42",
+    "url": "/robofriends/static/css/main.925a7a0e.chunk.css"
+  },
+  {
+    "revision": "25119274e15678301741",
+    "url": "/robofriends/static/css/1.40fdba95.chunk.css"
+  },
+  {
+    "revision": "1590b888ceedf9c7cb13f194ba0ad0eb",
+    "url": "/robofriends/index.html"
+  }
+];
